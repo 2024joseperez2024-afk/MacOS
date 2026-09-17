a29d69c359acca96c6f700289344e6bfffbae2e4
2024joseperez2024-afk <2024joseperez2024@gmail.com>
2026-09-17 19:02:36 -0400

    Update nido.dart

--- lib/screens/nido.dart
+++ lib/screens/nido.dart
@@ -67,7 +67,7 @@ class _NidoScreenState extends State<NidoScreen> {
   static const int defaultRpcCount = 9;
   final FlutterSecureStorage secureStorage = FlutterSecureStorage(
   mOptions: const MacOsOptions(
-    usecDataProtectionKeychain: false,
+    usesDataProtectionKeychain: false,
   ),
 );
   final ScrollController _scrollController = ScrollController();
